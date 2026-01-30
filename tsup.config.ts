149e2b70b8f33a7a3b10f6b483dc5b6f5e842578
Kassaila <kasela.kassaila@gmail.com>
2026-01-30 14:37:50 +0200

    wip: bundle update

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'tsup';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
   clean: true,
   treeshake: true,
+  sourcemap: isDev,
+  minify: !isDev,
+  external: ['vue-router'],
+  target: 'es2020',
 });
